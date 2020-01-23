15eba1b8ebcc1c105d68ac826f79d80f3dcf20a0
Zachary Nelson <nelsonz2013@hotmail.com>
2020-01-23 12:14:32 -0600

    first commit

---
+++ assets/script.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+    $(".owl-carousel").owlCarousel({
+        navigation : false, // Show next and prev buttons
+        autoplay:true,
+        slideSpeed : 300,
+        paginationSpeed : 400,
+        loop: true,
+        items : 1, 
+        itemsDesktop : false,
+        itemsDesktopSmall : false,
+        itemsTablet: false,
+        itemsMobile : false
+   
+    });
+  });
+
+  $(document).ready(function() {
+    $(".second-owl").owlCarousel({
+        navigation : false, // Show next and prev buttons
+        autoplay:true,
+        slideSpeed : 300,
+        paginationSpeed : 400,
+        loop: true,
+        items : 1, 
+        itemsDesktop : false,
+        itemsDesktopSmall : false,
+        itemsTablet: false,
+        itemsMobile : false
+   
+    });
+  });
